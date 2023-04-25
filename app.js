82b710689c2c9c7866b7f15e7eacf74e9a26e7b9
piyushgithub15 <piyush.github15@gmail.com>
2023-04-25 19:21:34 +0530

    second commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use("/api/employee", employeeRoute);
 
-//const mongoose = require('mongoose')
+
 mongoose.set('strictQuery',false);
 mongoose.connect('mongodb://127.0.0.1:27017/employeeDirectory', 
 {   useNewUrlParser: true, 
